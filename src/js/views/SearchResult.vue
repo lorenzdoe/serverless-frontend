99501de246a4c772198aaa1ee19fa95fee527d49
Lorenz Doellinger <if21b084@technikum-wien.at>
2024-01-24 18:56:30 +0100

    updated video creation

--- src/js/views/SearchResult.vue
+++ src/js/views/SearchResult.vue
@@ -15,7 +15,15 @@ const slots = ref([]);      // selected slots
 const date = ref('');       // selected date
 const hour = ref('');       // selected hour
 const image_urls = ref([])  // video data for selected date and hour
+const active_image = ref(0) // index of active image
 const plot_urls = ref([])   // plot data for selected date and hour  
+
+// update the active image every 2 sec
+setInterval(() => {
+    if (image_urls.value.length > 0) {
+        active_image.value = (active_image.value + 1) % image_urls.value.length;
+    }
+}, 2000);
 
 const fetchSlots = async () => {
     try {
@@ -79,22 +87,7 @@ onMounted(() => {
                 <button class="btn btn-primary btn-sm" v-bind:class="{'disabled': !(date && hour)}" v-on:click="fetchData(query,date,hour)">show</button>
             </div>
             <div id="carouselExampleControls" class="carousel slide mt-3" v-bind:class="{'d-none': image_urls.length === 0}" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item border active">
-                        <img :src="image_urls[0]" class="" alt="...">
-                    </div>
-                    <div class="carousel-item border" v-for="(url, index) in image_urls.slice(1)" :key="index">
-                        <img :src="url" class="" alt="...">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+                <img :src="image_urls[active_image]" class="d-block w-100" alt="...">
             </div>
 
             <div id="carouselExampleControls" class="carousel slide mt-3" v-bind:class="{'d-none': plot_urls.length === 0}" data-bs-ride="carousel">
